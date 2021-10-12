2cb507d1dc1f056a837822c21835a50eabf2e062
Mohak <mohak.gogia@quovantis.com>
2021-10-12 19:04:33 +0530

    Added CRUD App using Entity Code First Approach

---
+++ CRUDOperationCodeFirst/Migrations/202110111243539_Candidate.cs
@@ -0,0 +1,33 @@
+﻿namespace CRUDOperationCodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Candidate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Candidates",
+                c => new
+                    {
+                        CandidateId = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Email = c.String(nullable: false),
+                        PhnNum = c.String(nullable: false),
+                        DOB = c.String(),
+                        Gender = c.String(nullable: false),
+                        UnivName = c.String(nullable: false),
+                        ClgName = c.String(nullable: false),
+                        Branch = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.CandidateId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Candidates");
+        }
+    }
+}
---
+++ CRUDOperationCodeFirst/Models/Min18Years.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUDOperationCodeFirst.Models
+{
+    public class Min18Years : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var candidate = (Candidate)validationContext.ObjectInstance;
+
+            if (candidate.DOB == null)
+                return new ValidationResult("Date of Birth is required.");
+
+            int candidateAge = Convert.ToInt32(candidate.DOB.Substring(5));
+            int age = 2021 - candidateAge;
+            return (age >= 18)
+                ? ValidationResult.Success
+                : new ValidationResult("Candidate should be at least 18 years old.");
+        }
+    }
+}
